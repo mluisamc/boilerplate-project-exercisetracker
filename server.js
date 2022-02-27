048f82aa077aa04182d2d56c57dbe1b4224613de
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-27 13:02:57 +0100

    save user

--- server.js
+++ server.js
@@ -2,15 +2,38 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  username:  String
+});
+
+let User = mongoose.model('User', userSchema);
+
+
+app.post('/api/users', function(req, res) {
+  var user = new User({username: req.body.username});
+
+  user.save(function(err, data) {
+    if (err) return console.error(err);
+    res.json({ username : data.username, _id: data._id})
+  });
+  
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
