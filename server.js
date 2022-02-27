3fdcdf9350f2c2527e15c10e3456b7e34f486654
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-27 15:02:40 +0100

    basic logs

--- server.js
+++ server.js
@@ -63,6 +63,16 @@ app.post('/api/users/:id/exercises', function(req, res) {
   
 });
 
+app.get('/api/users/:id/logs', function(req,res) {
+  User.findById({_id: req.params.id}, function (err, user) {
+    if (err) return console.log(err);
+    Exercise.find({userId: req.params.id}, function (err, exercises) {
+      if (err) return console.log(err);
+      res.json({_id:req.params.id, username:user.username, count:exercises.count, log:exercises})
+    });
+  });      
+});
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
